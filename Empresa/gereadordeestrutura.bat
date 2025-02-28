2dd8f3882e71ed6d43ba21da85be37d096343944
Maria Dulce <madumoraesmartins@gmail.com>
2025-02-28 15:00:21 -0300

    atividade 28/02

---
+++ Empresa/gereadordeestrutura.bat
@@ -0,0 +1,10 @@
+mkdir ITAPETININGA\SETORES
+cd ITAPETININGA
+cd SETORES
+mkdir compras contabilidade financeiro fiscal RH TI vendas
+cd ..
+cd ..
+mkdir TATUI\SETORES
+cd TATUI
+cd SETORES
+mkdir compras contabilidade financeiro fiscal RH TI vendas
